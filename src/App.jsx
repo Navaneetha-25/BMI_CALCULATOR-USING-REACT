b338666a9632d8b6f49bb0121f07abd5edfb8481
Navaneetha-25 <navaneethac2006@gmail.com>
2026-02-23 10:36:21 +0530

    Refactor App component to include BMICalculator
    Removed routing and transport components, added BMICalculator.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-import NavBar from './components/Transport/NavBar'
-import Car from './components/Transport/Car'
-import Bike from './components/Transport/Bike'
-import Truck from './components/Transport/Truck'
-import Home from './components/Transport/Home'
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <h2>Welcome select any Transport </h2>
-      <Routes>
-        
-        <Route path="/" element={<Home/>} />
-        <Route path="/cars" element={<Car />} />
-        <Route path="/bikes" element={<Bike />} />
-        <Route path="/trucks" element={<Truck />} />
-      </Routes>
-    </Router>
+    <BMICalculator/>
   )
 }
 
